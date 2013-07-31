14d74b5ff504ce35368880c688538f0bd8d257b0
FajitaNachos <andrew@fajitanachos.com>
2013-07-31 15:00:09 +0900

    Filter Doorkeeper ajax results to get upcoming events and next event

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function(){
+  var upcomingEvents = [];
+  var nextEvent;
   $.ajax({
     type: "GET",
     url: "http://api.doorkeeper.jp/groups/hnkansai/events",
@@ -7,7 +9,20 @@ $(document).ready(function(){
     dataType: "jsonp",
     crossDomain: true,
     success: function(data){
-      console.log(data);
+      var today = new Date();
+      for (var i in data) {
+        if(new Date(data[i].event.starts_at) >= today)
+        upcomingEvents.push(data[i])
+      }
+
+     upcomingEvents.sort(function(a,b){
+      return Math.abs(1 - new Date(a.event.starts_at) / today) - Math.abs(1 - new Date(b.event.starts_at) / today)
+      });
+     nextEvent = upcomingEvents[1];
     }
-    });
+  });
+
+
+
+  
 });
