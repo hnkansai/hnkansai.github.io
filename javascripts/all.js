3c9d057537c6504f4cbf976d48a4aad78f970f12
FajitaNachos <andrew@fajitanachos.com>
2013-08-26 21:06:46 +0900

    Automated commit at 2013-08-26 12:06:46 UTC by middleman-deploy 3.1.4

--- javascripts/all.js
+++ javascripts/all.js
@@ -4519,12 +4519,11 @@ window.matchMedia=window.matchMedia||(function(e,f){var c,a=e.documentElement,b=
 
 
 $(document).ready(function(){
-
-    var photoSets = new Object();
-    var photoArray = new Object();
-    $('#navbar').affix({
-      offset: 129
-    });
+  var photoSets = new Object();
+  var photoArray = new Object();
+  $('#navbar').affix({
+    offset: 129
+  });
   //Flickr API calls
   var listData= {
     format: 'json',
@@ -4536,22 +4535,23 @@ $(document).ready(function(){
     var sets = data.photosets.photoset;
     var j = 1;
       for(var i=0;i<sets.length;i++){
-
         var photoSetId = sets[i].id;
         var photoSetTitle = sets[i].title._content;
-        var containerDiv = $('<div/>').attr("id", photoSetTitle).attr({
-                                                      class: "span4 gallery-img",
-                                                      'data-id': photoSetId
-                                                    });
+        var containerDiv = $('<div/>').attr({
+          id: photoSetTitle,
+          class: "span4 gallery-img",
+          'data-id': photoSetId
+        });
         var primaryImgSrc = "http://farm"+sets[i].farm+".staticflickr.com/"+sets[i].server+"/"+sets[i].primary+"_"+sets[i].secret+"_n.jpg"
         var primaryImg = $('<img/>').attr("src", primaryImgSrc);
         var primaryImgLrg =  "http://farm"+sets[i].farm+".staticflickr.com/"+sets[i].server+"/"+sets[i].primary+"_"+sets[i].secret+"_z.jpg"
        
         var a = $('<a/>').attr({
-          class: 'fancybox',
-          rel: photoSetId,
-          href: primaryImgLrg
+            class: 'fancybox',
+            rel: photoSetId,
+            href: primaryImgLrg
         });
+
         var caption = $('<div/>');
         caption.addClass('caption');
         caption.html('<h3>'+photoSetTitle  + '</h3>');
@@ -4568,8 +4568,8 @@ $(document).ready(function(){
      setTimeout(lazyLoadPhotos(), 2000);
    });
 
-    var date = new Date();
-    var today = date.toISOString(); 
+  var date = new Date();
+  var today = date.toISOString(); 
   //Doorkeeper API Call to fetch current events
   $.ajax({
     type: "GET",
@@ -4581,46 +4581,43 @@ $(document).ready(function(){
       var upcomingEvents = [];
       var nextEvent;
       for (var i in data) {
-      upcomingEvents.push(data[i].event);
-      nextEvent = data[0].event;
+        upcomingEvents.push(data[i].event);
+        nextEvent = data[0].event;
+      }
+      var formattedDate = formatDate(new Date(nextEvent.starts_at));
+       $('#events').append('<li id = "english-details">Next Event: </li><li><a href="http://hnkansai.doorkeeper.jp/events/'+nextEvent.id +'">'+nextEvent.title + ", " + formattedDate+'</a></li>');
     }
-    var formattedDate = formatDate(new Date(nextEvent.starts_at));
-     $('#events').append('<li id = "english-details">Next Event: </li><li><a href="http://hnkansai.doorkeeper.jp/events/'+nextEvent.id +'">'+nextEvent.title + ", " + formattedDate+'</a></li>');
-  }
   });
 
-
-
-
-function lazyLoadPhotos(){    
-  for (var id in photoSets){
-    if (photoSets.hasOwnProperty(id)){
-      var photoData = {
-        format: 'json',
-        method: 'flickr.photosets.getPhotos',
-        user_id: '99688089@N06',
-        photoset_id: photoSets[id],
-        api_key: '72a77248081016485a20c2b18c9c50ee'
-      }
-           
-      callFlickr(photoData,function(data){
-       
-        var photos = data.photoset.photo
-        var galleryDiv = $('*[data-id="'+data.photoset.id+'"]');
-     
-        for(var j=1;j<photos.length;j++){
-          var imgSrc = "http://farm"+photos[j].farm+".staticflickr.com/"+photos[j].server+"/"+photos[j].id+"_"+photos[j].secret+"_z.jpg"
-           var a = $('<a/>').attr({
-            class: 'fancybox',
-            rel: data.photoset.id,
-            href: imgSrc
-          });
-            galleryDiv.append(a);
-
+  function lazyLoadPhotos(){    
+    for (var id in photoSets){
+      if (photoSets.hasOwnProperty(id)){
+        var photoData = {
+          format: 'json',
+          method: 'flickr.photosets.getPhotos',
+          user_id: '99688089@N06',
+          photoset_id: photoSets[id],
+          api_key: '72a77248081016485a20c2b18c9c50ee'
         }
-      });
+             
+        callFlickr(photoData,function(data){
+         
+          var photos = data.photoset.photo
+          var galleryDiv = $('*[data-id="'+data.photoset.id+'"]');
+       
+          for(var j=1;j<photos.length;j++){
+            var imgSrc = "http://farm"+photos[j].farm+".staticflickr.com/"+photos[j].server+"/"+photos[j].id+"_"+photos[j].secret+"_z.jpg"
+            var a = $('<a/>').attr({
+              class: 'fancybox',
+              rel: data.photoset.id,
+              href: imgSrc
+            });
+            galleryDiv.append(a);
+          }
+        });
+      }
     }
-  }
+
     $('.gallery-img').hover(function(){  
         $(this).find('.caption').fadeIn('fast'); 
     }, function() {  
@@ -4631,11 +4628,11 @@ function lazyLoadPhotos(){
     $('#galleries .caption').click(function(event){ 
         $(this).prev().click();
     });
-}
+  }
 
 
-function callFlickr(data, callback){
-   $.ajax({
+  function callFlickr(data, callback){
+    $.ajax({
       type: "GET",
       url: "http://api.flickr.com/services/rest/",
       data: data,
@@ -4645,68 +4642,65 @@ function callFlickr(data, callback){
         callback(data);
       },
       error: function(){
-        
       }
- });
-}
-
-
-function formatDate(date){
-
-  var months = new Array("January", "February", "March", 
-  "April", "May", "June", "July", "August", "September", 
-  "October", "November", "December");
-
-
-  var dayOfMonth = date.getDate();
-  var month = date.getMonth();
-  var hour = date.getHours();
-  var minutes = date.getMinutes();
-  var suffix = "";
-  var am_pm;
-
-  //add the proper suffix (st, nd, rd) to the numerical day of the month
-  switch(dayOfMonth) {
-    case dayOfMonth == 1:
-    case dayOfMonth == 21: 
-    case dayOfMonth == 31:
-     suffix = "st";
-     break;
-    case dayOfMonth == 2:
-    case dayOfMonth == 22:
-     suffix = "nd";
-     break;
-    case dayOfMonth == 3: 
-    case dayOfMonth == 23:
-     suffix = "rd";
-     break;
-    default:
-    suffix = "th";
+    });
   }
 
-  if (minutes == 0){
-    minutes = '00';
+
+  function formatDate(date){
+    var months = new Array("January", "February", "March", 
+      "April", "May", "June", "July", "August", "September", 
+      "October", "November", "December"
+    );
+
+    var dayOfMonth = date.getDate();
+    var month = date.getMonth();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var suffix = "";
+    var am_pm;
+
+    //add the proper suffix (st, nd, rd) to the numerical day of the month
+    switch(dayOfMonth) {
+      case dayOfMonth == 1:
+      case dayOfMonth == 21: 
+      case dayOfMonth == 31:
+       suffix = "st";
+       break;
+      case dayOfMonth == 2:
+      case dayOfMonth == 22:
+       suffix = "nd";
+       break;
+      case dayOfMonth == 3: 
+      case dayOfMonth == 23:
+       suffix = "rd";
+       break;
+      default:
+      suffix = "th";
+    }
+
+    if (minutes == 0){
+      minutes = '00';
+    }
+    //format the time into 12 hour format with proper AM PM
+    if (hour < 12)
+       {
+       am_pm = "AM";
+       }
+    else
+       {
+       am_pm = "PM";
+       }
+
+    if (hour == 0){
+        hour = 12;
+       }
+    if (hour > 12){
+        hour = hour - 12;
+       }
+
+    return months[month] +" "+ dayOfMonth + suffix + ", " + hour + ":" + minutes + " " + am_pm;
   }
-  //format the time into 12 hour format with proper AM PM
-  if (hour < 12)
-     {
-     am_pm = "AM";
-     }
-  else
-     {
-     am_pm = "PM";
-     }
-
-  if (hour == 0){
-      hour = 12;
-     }
-  if (hour > 12){
-      hour = hour - 12;
-     }
-
-return months[month] +" "+ dayOfMonth + suffix + ", " + hour + ":" + minutes + " " + am_pm;
-
-}
 
 
 
@@ -4719,189 +4713,172 @@ http://www.apache.org/licenses/LICENSE-2.0
 
 */
   (function() {
-      function createPlayer(jqe, video, options) {
-          var ifr = $('iframe', jqe);
-          if (ifr.length === 0) {
-              ifr = $('<iframe scrolling="no">');
-              ifr.addClass('player');
-          }
-          var src = 'http://www.youtube.com/embed/' + video.id;
-          if (options.playopts) {
-              src += '?';
-              for (var k in options.playopts) {
-                  src += k + '=' + options.playopts[k] + '&';
-              }
-              src += '_a=b';
-          }
-          $('.video-title').html(video.title);
-          $('.video-description').html(video.desc);
-          ifr.attr('src', src);
-          jqe.prepend(ifr);
+    function createPlayer(jqe, video, options) {
+        var ifr = $('iframe', jqe);
+        if (ifr.length === 0) {
+            ifr = $('<iframe scrolling="no">');
+            ifr.addClass('player');
+        }
+        var src = 'http://www.youtube.com/embed/' + video.id;
+        if (options.playopts) {
+            src += '?';
+            for (var k in options.playopts) {
+                src += k + '=' + options.playopts[k] + '&';
+            }
+            src += '_a=b';
+        }
+        $('.video-title').html(video.title);
+        $('.video-description').html(video.desc);
+        ifr.attr('src', src);
+        jqe.prepend(ifr);
 
+    }
+
+    function createCarousel(jqe, videos, options) {
+      var car = $('div.video-carousel', jqe);
+      if (car.length === 0) {
+        car = $('<div>');
+        car.addClass('video-carousel');
+        var carouselWrap = $('<div/>');
+        carouselWrap.addClass('carousel-wrap');
+        carouselWrap.append(car)
+        jqe.append(carouselWrap);
       }
 
-      function createCarousel(jqe, videos, options) {
-          var car = $('div.video-carousel', jqe);
-          if (car.length === 0) {
-              car = $('<div>');
-              car.addClass('video-carousel');
-              var carouselWrap = $('<div/>');
-              carouselWrap.addClass('carousel-wrap');
-              carouselWrap.append(car)
-              jqe.append(carouselWrap);
-          }
-
-          $.each(videos, function(i, video) {
-              options.thumbnail(car, video, options);
-          });
-      }
-
-      function createDetails(jqe, video, options){
-        var details = $('div.video-details', jqe);
-         if (details.length === 0) {
-              details = $('<div/>');
-              details.addClass('video-details');
-              title = $('<h3/>');
-              title.addClass('video-title');
-              desc = $('<p/>');
-              desc.addClass('video-description');
-              details.append(title);
-              details.append(desc);
-              jqe.prepend(details);
-          }
-
-      }
-
-
-      function createThumbnail(jqe, video, options) {
-        
-          var imgurl = video.thumbnails[0].url;
-          var img = $('img[src="' + imgurl + '"]');
-          var desc;
-          var container;
-          var mycontainer = $('<div/>');
-          mycontainer.addClass('thumb-container');  
-          if (img.length !== 0) return;
-          img = $('<img/>');
-          img.addClass('thumbnail');
-          mycontainer.append(img);
-          img.attr('src', imgurl);
-          img.attr('title', video.title);
-          desk = $('<p class="yt-title">' + video.title + '</p>');
-          mycontainer.append(desk);
-          jqe.append(mycontainer);
-          
-          mycontainer.click(function() {
-              options.player(options.maindiv, video, $.extend(true, {}, options, {
-                  playopts: {
-                      autoplay: 1
-                  }
-              }));
-             
-          });
-      }
-
-      var defoptions = {
-          autoplay: false,
-          user: null,
-          carousel: createCarousel,
-          details: createDetails,
-          player: createPlayer,
-          thumbnail: createThumbnail,
-          loaded: function() {
-            var thumbs = $('.thumb-container').length;
-            var thumbWidth = $('.thumb-container:first').width() + 40;
-            var containerWidth = thumbs * thumbWidth;
-            var windowWidth = $(window).width();
-       
-            $('.video-carousel').width(containerWidth);
-          },
-          playopts: {
-              autoplay: 0,
-              egm: 1,
-              autohide: 1,
-              fs: 1,
-              showinfo: 1
-          }
-      };
-
-
-      $.fn.extend({
-          youTubeChannel: function(options) {
-              var md = $(this);
-              md.addClass('youtube');
-              md.addClass('youtube-channel');
-              var allopts = $.extend(true, {}, defoptions, options);
-              allopts.maindiv = md;
-              $.getJSON('http://gdata.youtube.com/feeds/api/users/' + allopts.user + '/uploads?alt=json-in-script&orderby=published&format=5&callback=?', null, function(data) {
-                  var feed = data.feed;
-                  var videos = [];
-               
-                  $.each(feed.entry, function(i, entry) {
-
-                      var video = {
-                          title: entry.title.$t,
-                          id: entry.id.$t.match('[^/]*$'),
-                          desc: entry.content.$t,
-                          thumbnails: entry.media$group.media$thumbnail
-                      };
-                      videos.push(video);
-                  });
-                  allopts.allvideos = videos;
-                  allopts.carousel(md, videos, allopts);
-                  allopts.details(md, videos[0], allopts);
-                  allopts.player(md, videos[0], allopts);
-                  allopts.loaded(videos, allopts);
-              });
-          }
+      $.each(videos, function(i, video) {
+          options.thumbnail(car, video, options);
       });
+    }
 
-    })();
+    function createDetails(jqe, video, options){
+      var details = $('div.video-details', jqe);
+       if (details.length === 0) {
+        details = $('<div/>');
+        details.addClass('video-details');
+        title = $('<h3/>');
+        title.addClass('video-title');
+        desc = $('<p/>');
+        desc.addClass('video-description');
+        details.append(title);
+        details.append(desc);
+        jqe.prepend(details);
+      }
+    }
+
+    function createThumbnail(jqe, video, options) {
+      
+        var imgurl = video.thumbnails[0].url;
+        var img = $('img[src="' + imgurl + '"]');
+        var desc;
+        var container;
+        var mycontainer = $('<div/>');
+        mycontainer.addClass('thumb-container');  
+        if (img.length !== 0) return;
+        img = $('<img/>');
+        img.addClass('thumbnail');
+        mycontainer.append(img);
+        img.attr('src', imgurl);
+        img.attr('title', video.title);
+        desk = $('<p class="yt-title">' + video.title + '</p>');
+        mycontainer.append(desk);
+        jqe.append(mycontainer);
+        
+        mycontainer.click(function() {
+            options.player(options.maindiv, video, $.extend(true, {}, options, {
+                playopts: {
+                    autoplay: 1
+                }
+            }));
+           
+        });
+    }
+
+    var defoptions = {
+      autoplay: false,
+      user: null,
+      carousel: createCarousel,
+      details: createDetails,
+      player: createPlayer,
+      thumbnail: createThumbnail,
+      loaded: function() {
+        var thumbs = $('.thumb-container').length;
+        var thumbWidth = $('.thumb-container:first').width() + 40;
+        var containerWidth = thumbs * thumbWidth;
+        var windowWidth = $(window).width();
+   
+        $('.video-carousel').width(containerWidth);
+      },
+      playopts: {
+          autoplay: 0,
+          egm: 1,
+          autohide: 1,
+          fs: 1,
+          showinfo: 1
+      }
+    };
+
+    $.fn.extend({
+      youTubeChannel: function(options) {
+        var md = $(this);
+        md.addClass('youtube');
+        md.addClass('youtube-channel');
+        var allopts = $.extend(true, {}, defoptions, options);
+        allopts.maindiv = md;
+        $.getJSON('http://gdata.youtube.com/feeds/api/users/' + allopts.user + '/uploads?alt=json-in-script&callback=?', null, function(data) {
+          var feed = data.feed;
+          var videos = [];
+       
+          $.each(feed.entry, function(i, entry) {
+            var video = {
+                title: entry.title.$t,
+                id: entry.id.$t.match('[^/]*$'),
+                desc: entry.content.$t,
+                thumbnails: entry.media$group.media$thumbnail
+            };
+            videos.push(video);
+          });
+          allopts.allvideos = videos;
+          allopts.carousel(md, videos, allopts);
+          allopts.details(md, videos[0], allopts);
+          allopts.player(md, videos[0], allopts);
+          allopts.loaded(videos, allopts);
+        });
+      }
+    });
+  })();
 
   $(function() {
-      $('#player').youTubeChannel({
-          user: 'UCbxKJC9zZJ9C7YDYuZ3m59Q'
-      });
-
-  });
-
-
-  $(".fancybox").fancybox({
-  
+    $('#player').youTubeChannel({
+        user: 'UCbxKJC9zZJ9C7YDYuZ3m59Q'
+    });
   });
 
   $('.organizer').hover(function(){  
-        $(".caption", this).fadeIn('fast');  
+      $(".caption", this).fadeIn('fast');  
     }, function() {  
-        $(".caption", this).fadeOut('fast'); 
-    });
-
-  var $root = $('html, body');
+      $(".caption", this).fadeOut('fast'); 
+  });
 
   $('#english').on('click', function(){
     $('.en').hide();
     $('.jp').show();
-    
-
   });
+
   $('#japanese').on('click', function(){
     $('.jp').hide();
     $('.en').show();
-   
   });
 
+  var $root = $('html, body');
   $('a').click(function() {
-      var href = $.attr(this, 'href');
-      $root.animate({
-          scrollTop: $(href).offset().top
-      }, 500, function () {
-          window.location.hash = href;
-      });
-      return false;
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
   });
-
-
-
 });
 
 
