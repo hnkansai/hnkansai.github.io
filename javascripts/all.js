589718bea16a8a1555798c926040358d79139a75
FajitaNachos <andrew@fajitanachos.com>
2013-08-26 14:47:49 +0900

    Automated commit at 2013-08-26 05:47:49 UTC by middleman-deploy 3.1.4

--- javascripts/all.js
+++ javascripts/all.js
@@ -4585,7 +4585,7 @@ $(document).ready(function(){
       nextEvent = data[0].event;
     }
     var formattedDate = formatDate(new Date(nextEvent.starts_at));
-     $('#event-details').append('<a href="http://hnkansai.doorkeeper.jp/events/'+nextEvent.id +'">'+nextEvent.title + ", " + formattedDate+'</a>');
+     $('#events').append('<li id = "english-details">Next Event: </li><li><a href="http://hnkansai.doorkeeper.jp/events/'+nextEvent.id +'">'+nextEvent.title + ", " + formattedDate+'</a></li>');
   }
   });
 
@@ -4878,6 +4878,18 @@ http://www.apache.org/licenses/LICENSE-2.0
 
   var $root = $('html, body');
 
+  $('#english').on('click', function(){
+    $('.en').hide();
+    $('.jp').show();
+    
+
+  });
+  $('#japanese').on('click', function(){
+    $('.jp').hide();
+    $('.en').show();
+   
+  });
+
   $('a').click(function() {
       var href = $.attr(this, 'href');
       $root.animate({
@@ -4887,6 +4899,8 @@ http://www.apache.org/licenses/LICENSE-2.0
       });
       return false;
   });
+
+
 
 });
 
